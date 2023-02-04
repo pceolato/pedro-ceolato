7072ae1563bc5ac7e477f23953a4576700aea0e7
Pedro <pedroceolato2002@gmail.com>
2023-02-04 14:59:51 -0300

    feat: added avatar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/no-unescaped-entities */
 import { GitHub, Linkedin } from 'react-feather'
+import * as Avatar from '@radix-ui/react-avatar'
 
 export function Home() {
   return (
     <div className="px-8 flex flex-col gap-6 items-start justify-between w-full md:w-[1072px] m-auto md:flex-row">
       <div className="max-w-screen-sm">
-        <div>
+        <div className="flex flex-col md:flex-row items-start md:items-end gap-4 md:gap-8">
+          <Avatar.Root className="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-full">
+            <Avatar.Image
+              className="w-full h-full object-cover rounded-full"
+              src="https://github.com/pceolato.png"
+              alt="foto do Pedro Ceolato"
+            />
+            <Avatar.Fallback
+              delayMs={100}
+              className="w-full h-full object-cover rounded-full bg-gray-900 text-teal-500 flex items-center justify-center text-4xl"
+            >
+              PC
+            </Avatar.Fallback>
+          </Avatar.Root>
           <h1 className="text-teal-500 font-bold text-3xl md:text-4xl lg:text-5xl">
             Hi, I'm Pedro Ceolato
           </h1>
@@ -20,7 +34,7 @@ export function Home() {
         </span>
         <a
           href="mailto:pedroceolato2002@gmail.com?"
-          className="text-teal-700 hover:text-teal-300 transition-all delay-100 text-sm md:text-base lg:text-xl"
+          className="text-teal-300 hover:text-teal-700 transition-all delay-100 text-sm md:text-base lg:text-xl"
         >
           pedroceolato2002@gmail.com
         </a>
