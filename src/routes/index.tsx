b82fcb70f8ac880e5708e2aeac656f2d42e165f0
Pedro <pedroceolato2002@gmail.com>
2023-02-20 20:36:35 -0300

    feat: add error path t opage notfound

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { MainLayout } from '../layouts/MainLayout'
 import { Home } from '../pages/Home'
+import { NotFound } from '../pages/NotFound'
 import { Projects } from '../pages/Projects'
 
 export function Router() {
@@ -10,6 +11,7 @@ export function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
