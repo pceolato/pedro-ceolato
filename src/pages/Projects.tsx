6729b31ee1387330cff13cb37636b4c5f08a1123
Pedro <pedroceolato2002@gmail.com>
2023-02-20 20:02:47 -0300

    refactor: all projects render

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,38 +1,48 @@
-import { Project } from '../components/project/Project'
+import { Project, ProjectProps } from '../components/project/Project'
 import portfolioImage from '../assets/portfolio.svg'
 import nbaImage from '../assets/nba-players.svg'
 
 export function Projects() {
+  const allProjects: ProjectProps[] = [
+    {
+      title: 'NBA Players',
+      description:
+        'Web application to fetch information about all NBA players.',
+      hrefRepository: 'https://github.com/pceolato/nba-players',
+      hrefDeploy: 'https://nba-players.vercel.app',
+      technologies: ['nextjs', 'tailwindcss'],
+      image: {
+        src: nbaImage,
+        nameProject: 'NBA-Players',
+      },
+    },
+    {
+      title: 'Pedro Ceolato',
+      description:
+        'Project developed to centralize some projects and collaborations and improve my skills.',
+      hrefRepository: 'https://github.com/pceolato/pedro-ceolato',
+      hrefDeploy: '/',
+      technologies: ['reactjs', 'tailwindcss'],
+      image: {
+        src: portfolioImage,
+        nameProject: 'pedro-ceolato',
+      },
+    },
+  ]
+
   return (
     <div className="w-full max-w-[950px] mx-auto px-8 flex flex-col items-center">
-      <Project
-        title="NBA Players"
-        description="Web application to fetch information about all NBA players."
-        hrefRepository="https://github.com/pceolato/nba-players"
-        hrefDeploy="https://nba-players.vercel.app"
-        technologies={['nextjs', 'tailwindcss']}
-        image={
-          <img
-            src={nbaImage}
-            alt="Imagem do projeto nba players"
-            className="w-full h-full object-cover rounded"
-          />
-        }
-      />
-      <Project
-        title="Pedro Ceolato"
-        description="Project developed to centralize some projects and collaborations and improve my skills."
-        hrefRepository="https://github.com/pceolato/pedro-ceolato"
-        hrefDeploy="/"
-        technologies={['reactjs', 'tailwindcss']}
-        image={
-          <img
-            src={portfolioImage}
-            alt="Imagem do projeto pedro ceolato"
-            className="w-full h-full object-cover rounded"
-          />
-        }
-      />
+      {allProjects.map((project, i) => (
+        <Project
+          key={`${project.title}-${i}`}
+          title={project.title}
+          description={project.description}
+          hrefDeploy={project.hrefDeploy}
+          hrefRepository={project.hrefRepository}
+          technologies={project.technologies}
+          image={project.image}
+        />
+      ))}
     </div>
   )
 }
