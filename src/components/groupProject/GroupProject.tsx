9c55e0df031e2441c5e857a3a37a04e8a4b5c051
Pedro Ceolato <pedroceolato2002@gmail.com>
2023-11-09 19:57:53 -0300

    feat: adjust responsive page projects

--- src/components/groupProject/GroupProject.tsx
+++ src/components/groupProject/GroupProject.tsx
@@ -26,6 +26,7 @@ interface GroupProjectProps {
   title: string
   description: string
   projects: ProjectProps[]
+  isLastProject: boolean
 }
 
 export function GroupProject({
@@ -33,51 +34,57 @@ export function GroupProject({
   title,
   description,
   projects,
+  isLastProject,
 }: GroupProjectProps) {
   return (
-    <div className="flex items-center justify-between w-full mb-24">
-      <div className="flex flex-col gap-4 max-w-xs">
-        <h3 className="font-light text-3xl text-title">{title}</h3>
-        <p className="font-light text-gray-400 text-lg">{description}</p>
-      </div>
-      <div className="max-w-md">
-        <Swiper
-          effect="flip"
-          modules={[
-            Navigation,
-            Pagination,
-            Scrollbar,
-            A11y,
-            EffectFlip,
-            Mousewheel,
-          ]}
-          spaceBetween={50}
-          loop={true}
-          slidesPerView={1}
-          mousewheel={true}
-          navigation
-          pagination={{ clickable: true, dynamicBullets: true }}
-        >
-          {projects.map((project, i) => (
-            <SwiperSlide key={`${project.title}-${i}`}>
-              <ProjectItem
-                title={project.title}
-                description={project.description}
-                hrefDeploy={project.hrefDeploy}
-                hrefRepository={project.hrefRepository}
-                technologies={project.technologies}
-                image={project.image}
-                typeProject={project.typeProject}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className="flex justify-end mt-6">
-          <Link className="text-title font-semibold" to={type}>
-            View All
-          </Link>
+    <>
+      <div className="flex flex-col md:flex-row items-center justify-between w-full ">
+        <div className="flex flex-col gap-4 max-w-xs mb-6 md:mb-0">
+          <h3 className="font-light text-3xl text-title">{title}</h3>
+          <p className="font-light text-gray-400 text-lg">{description}</p>
+        </div>
+        <div className="w-full max-w-md">
+          <Swiper
+            effect="flip"
+            modules={[
+              Navigation,
+              Pagination,
+              Scrollbar,
+              A11y,
+              EffectFlip,
+              Mousewheel,
+            ]}
+            spaceBetween={50}
+            loop={true}
+            slidesPerView={1}
+            mousewheel={true}
+            navigation
+            pagination={{ clickable: true, dynamicBullets: true }}
+          >
+            {projects.map((project, i) => (
+              <SwiperSlide key={`${project.title}-${i}`}>
+                <ProjectItem
+                  title={project.title}
+                  description={project.description}
+                  hrefDeploy={project.hrefDeploy}
+                  hrefRepository={project.hrefRepository}
+                  technologies={project.technologies}
+                  image={project.image}
+                  typeProject={project.typeProject}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="flex justify-end mt-6">
+            <Link className="text-title font-semibold" to={type}>
+              View All
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      {!isLastProject && (
+        <div className="p-[1px] bg-gray-400 my-24 w-1/2  px-14" />
+      )}
+    </>
   )
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,13 +3,14 @@ import { allProjects } from '../utils/allProjects'
 
 export function Projects() {
   return (
-    <div className="w-full max-w-[950px] mx-auto px-8 flex flex-col items-center">
+    <div className="w-full max-w-[950px] mx-auto px-8 flex flex-col items-center mb-24">
       {allProjects.map((project, i) => (
         <GroupProject
           key={`${project.type}-${i}`}
           type={project.type}
           title={project.title}
           description={project.description}
+          isLastProject={allProjects.length - 1 === i}
           projects={project.projects}
         />
       ))}
