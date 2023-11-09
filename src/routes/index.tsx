c7f83eb909198ca030794727440f32772b1b2ca1
Pedro Ceolato <pedroceolato2002@gmail.com>
2023-11-09 18:59:21 -0300

    feat: adjust routes path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,17 @@ import { MainLayout } from '../layouts/MainLayout'
 import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
 import { Projects } from '../pages/Projects'
+import { ProjectsByType } from '../pages/ProjectsByType'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects">
+          <Route index element={<Projects />} />
+          <Route path="/projects/:typeProject" element={<ProjectsByType />} />
+        </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
