c2001db902ca972999e4f1791442c9797198f063
Pedro Ceolato <pedroceolato2002@gmail.com>
2023-11-09 19:00:47 -0300

    feat: updated navItem isActive

--- src/components/navbar/NavDesktop.tsx
+++ src/components/navbar/NavDesktop.tsx
@@ -1,14 +1,21 @@
 import { Logo } from './Logo'
 import { NavLink } from './NavLink'
 import { ToggleIcon } from '../ToggleIcon'
+import { useLocation } from 'react-router-dom'
 
 export function NavDesktop() {
+  const { pathname } = useLocation()
+
   return (
     <div className="flex justify-between max-w-[1072px] mx-auto p-8 pb-36">
       <Logo />
       <nav className="flex items-center gap-12">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/projects">Projects</NavLink>
+        <NavLink href="/" isActive={pathname === '/'}>
+          Home
+        </NavLink>
+        <NavLink href="/projects" isActive={pathname.startsWith('/projects')}>
+          Projects
+        </NavLink>
         <ToggleIcon />
       </nav>
     </div>
