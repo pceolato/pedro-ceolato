201f3428f4d18a9aa3cda45eb2596844b96121e0
Pedro Ceolato <pedroceolato2002@gmail.com>
2024-01-02 19:52:46 -0300

    feat: added tabs in projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,19 +1,53 @@
-import { GroupProject } from '../components/groupProject/GroupProject'
 import { allProjects } from '../utils/allProjects'
+import * as Tabs from '@radix-ui/react-tabs'
+import '../styles/projects.css'
+import { Project } from '../components/project/Project'
 
 export function Projects() {
   return (
-    <div className="w-full max-w-[950px] mx-auto px-8 flex flex-col items-center mb-24">
-      {allProjects.map((project, i) => (
-        <GroupProject
-          key={`${project.type}-${i}`}
-          type={project.type}
-          title={project.title}
-          description={project.description}
-          isLastProject={allProjects.length - 1 === i}
-          projects={project.projects}
-        />
-      ))}
+    <div className="w-full max-w-[950px] mx-auto px-8 flex flex-col mb-24">
+      <Tabs.Root defaultValue="personal" className="text-title">
+        <Tabs.List className="flex items-center gap-6 font-extralight text-xl mb-20">
+          <Tabs.Trigger
+            className="hover:translate-y-1 transition-all tabsTrigger"
+            value="personal"
+          >
+            Personal
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            className="hover:translate-y-1 transition-all tabsTrigger"
+            value="cothefato"
+          >
+            Cothefato Studio
+          </Tabs.Trigger>
+        </Tabs.List>
+        {allProjects.map((project, index) => (
+          <Tabs.Content
+            key={project.type}
+            value={project.type}
+            className="flex flex-col gap-16"
+          >
+            <p className="max-w-4xl text-center text-gray-400">
+              {project.description}
+            </p>
+
+            <div className="w-full">
+              {project.projects.map((project, i) => (
+                <Project
+                  key={`${project.title}-${i}`}
+                  title={project.title}
+                  description={project.description}
+                  hrefDeploy={project.hrefDeploy}
+                  hrefRepository={project.hrefRepository}
+                  technologies={project.technologies}
+                  images={project.images}
+                  typeProject={project.typeProject}
+                />
+              ))}
+            </div>
+          </Tabs.Content>
+        ))}
+      </Tabs.Root>
     </div>
   )
 }
